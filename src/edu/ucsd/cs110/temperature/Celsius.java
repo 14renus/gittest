c11fed27d63b4c09c77316b3d4a668d58de0f141
Renu Singh <res001@ucsd.edu>
2016-04-25 11:13:18 -0700

    config files

--- src/edu/ucsd/cs110/temperature/Celsius.java
+++ src/edu/ucsd/cs110/temperature/Celsius.java
@@ -10,10 +10,22 @@ public class Celsius extends Temperature {
             super(t);
         }
 
+    @Override
+    public Temperature toCelsius() {
+        return this;
+    }
 
-        public String toString()
+    @Override
+    public Temperature toFahrenheit() {
+        float temp = getValue();
+        temp = (temp*(9/5))+32;
+        return new Fahrenheit(temp);
+    }
+
+
+    public String toString()
         {
             // TODO: Complete this method
-            return "";
+            return Float.toString(getValue());
         }
 }
--- src/edu/ucsd/cs110/temperature/Fahrenheit.java
+++ src/edu/ucsd/cs110/temperature/Fahrenheit.java
@@ -9,12 +9,23 @@ public class Fahrenheit extends Temperature {
         super(t);
     }
 
+    @Override
+    public Temperature toCelsius() {
+        float temp = getValue();
+        temp = (temp - 32)*(5/9);
+        return new Celsius(temp);
+    }
+
+    @Override
+    public Temperature toFahrenheit() {
+        return this;
+    }
 
 
     public String toString()
     {
         // TODO: Complete this method
-        return "";
+        return Float.toString(value);
     }
 }
 
